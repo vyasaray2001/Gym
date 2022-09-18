faafa73f721a43407d8ecfd83a3e215cc32faf89
vyasaray2001 <99123105+vyasaray2001@users.noreply.github.com>
2022-09-18 20:38:54 +0530

    Update login.php

--- login.php
+++ login.php
@@ -9,6 +9,7 @@ require_once 'include\dbmscon.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Login</title>
+    <link rel="shortcut icon" href="Web/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="css/loginstyle.css">
 </head>
 <body>
